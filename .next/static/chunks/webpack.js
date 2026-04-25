c22a517e071ccfa00d63f3aa4ef611600efb2586
jadesainsbury1 <jade.sainsbury1@gmail.com>
2026-04-25 21:42:59 +0200

    fix webhook dynamic export

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "42b07c44a0d514d9"; }
+/******/ 		__webpack_require__.h = function() { return "93aad33dd10a3f87"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
