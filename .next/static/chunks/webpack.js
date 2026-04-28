2a2ef33843d768326cba74df7ce758c9a9cd4762
jadesainsbury1 <jade.sainsbury1@gmail.com>
2026-04-28 17:44:45 +0200

    add sitemap

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0985c6c2683e0f35"; }
+/******/ 		__webpack_require__.h = function() { return "9973d7fde50c0381"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
