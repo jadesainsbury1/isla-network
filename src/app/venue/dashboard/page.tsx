e8d003e2de7c2dd0606440c490aba8307c850c61
jadesainsbury1 <jade.sainsbury1@gmail.com>
2026-04-29 21:45:47 +0200

    add packages section to venue dashboard

--- src/app/venue/dashboard/page.tsx
+++ src/app/venue/dashboard/page.tsx
@@ -4,6 +4,7 @@ import BookingConfirm from '@/components/BookingConfirm'
 import BookingMessage from '@/components/BookingMessage'
 import VenuePayButton from '@/components/VenuePayButton'
 import BillUpload from '@/components/BillUpload'
+import VenuePackages from '@/components/VenuePackages'
 import type { Booking, Profile } from '@/lib/types'
 
 export default async function VenueDashboardPage() {
@@ -207,6 +208,12 @@ export default async function VenueDashboardPage() {
             )}
           </div>
         )}
+
+        {/* Packages & Offers */}
+        <div className="card" style={{ padding: 24, marginBottom: 24 }}>
+          <VenuePackages venueId={venue.id} initial={(venue as any).packages || []} />
+        </div>
+
 
       </div>
     </>
